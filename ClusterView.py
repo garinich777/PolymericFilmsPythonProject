518a9c0228262e6b062a9a8fd14367f012193f46
Igor Osipov <62898482+garinich777@users.noreply.github.com>
2021-12-28 00:56:28 +0300

    Last ver

--- ClusterView.py
+++ ClusterView.py
@@ -1,6 +1,4 @@
 import numpy as np
-
-
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets import make_blobs
@@ -8,7 +6,8 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 def Start(data, eps = 0.25, min_samples = 10):
-
+    # #############################################################################
+    # Generate sample data
     centers = [[1, 1], [-1, -1], [1, -1]]
     # X, labels_true = make_blobs(
     #     n_samples=700, centers=centers, cluster_std=0.5, random_state=2
@@ -16,24 +15,27 @@ def Start(data, eps = 0.25, min_samples = 10):
 
     X = StandardScaler().fit_transform(data)
 
+    # #############################################################################
+    # Compute DBSCAN
     db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
 
+    # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
-    print("Estimated number of clusters: %d" % n_clusters_)
-    print("Estimated number of noise points: %d" % n_noise_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-    print(
-        "Adjusted Mutual Information: %0.3f"
-        % metrics.adjusted_mutual_info_score(labels_true, labels)
-    )
+    # print("Estimated number of clusters: %d" % n_clusters_)
+    # print("Estimated number of noise points: %d" % n_noise_)
+    # print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    # print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    # print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    # print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+    # print(
+    #     "Adjusted Mutual Information: %0.3f"
+    #     % metrics.adjusted_mutual_info_score(labels_true, labels)
+    # )
     #print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
 
     # #############################################################################
--- Compression.py
+++ Compression.py
@@ -1,6 +1,5 @@
-import numpy as np
-import umap as u
+import umap.umap_ as u
 
 def CompressingData(data, n = 5):
-    embedding = u.UMAP(n_neighbors=5).fit_transform(data)
+    embedding = u.UMAP(n_neighbors=30, min_dist=0.1, ).fit_transform(data)
     return embedding
